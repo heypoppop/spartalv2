c97b9aeb1cf6d6b7876580e0047bee7a85b5cf12
heypop <hey@herenas.com>
2023-09-03 07:08:40 +0900

    로그인 기능 구현

--- src/main/java/com/sparta/blog/security/JwtAuthenticationFilter.java
+++ src/main/java/com/sparta/blog/security/JwtAuthenticationFilter.java
@@ -43,19 +43,31 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     }
 
     @Override
-    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) {
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException {
         String username = ((UserDetailsImpl) authResult.getPrincipal()).getUsername();
         UserRoleEnum role = ((UserDetailsImpl) authResult.getPrincipal()).getUser().getRole();
 
         String token = jwtUtil.createToken(username, role);
         response.addHeader(JwtUtil.AUTHORIZATION_HEADER, token);
+
+        // 로그인 성공 메세지 전달
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("UTF-8");
+        String massage = "로그인 성공";
+        response.getWriter().write("상태코드 : " + response.getStatus() +", 메세지 : " +  massage);
     }
 
     @Override
-    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) {
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException {
 
-        // 오류 내용 담아서 보내줄 수 있음
+        // 오류 메세지 전달
+        String errorMessage = failed.getMessage();
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/plain");
         response.setStatus(401);
+        response.getWriter().write("상태코드 : " + response.getStatus() +", 메세지 : " + errorMessage);
+
+
     }
 
 }
